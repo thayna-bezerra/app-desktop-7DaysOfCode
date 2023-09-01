e1f6fd7182ddfb5694ca26537b5220e85cd12ac9
thayna <arraisthayna2106@gmail.com>
2023-08-31 23:36:21 -0300

    welcome screen

--- pet-as-service/Form1.Designer.cs
+++ pet-as-service/Form1.Designer.cs
@@ -29,13 +29,84 @@ namespace pet_as_service
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.buscarRaçasStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.meusFavoritosStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.buscarRaçasStripMenuItem,
+            this.meusFavoritosStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(354, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // buscarRaçasStripMenuItem
+            // 
+            this.buscarRaçasStripMenuItem.Name = "buscarRaçasStripMenuItem";
+            this.buscarRaçasStripMenuItem.Size = new System.Drawing.Size(87, 20);
+            this.buscarRaçasStripMenuItem.Text = "Buscar Raças";
+            // 
+            // meusFavoritosStripMenuItem
+            // 
+            this.meusFavoritosStripMenuItem.Name = "meusFavoritosStripMenuItem";
+            this.meusFavoritosStripMenuItem.Size = new System.Drawing.Size(99, 20);
+            this.meusFavoritosStripMenuItem.Text = "Meus Favoritos";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::pet_as_service.Properties.Resources.cat;
+            this.pictureBox1.Location = new System.Drawing.Point(109, 65);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(139, 140);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(63, 230);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(240, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Welcome to Cat As Service";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(354, 306);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem buscarRaçasStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem meusFavoritosStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
